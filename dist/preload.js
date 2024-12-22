6e7e70fe58cb2e13f6979d0eaea6a5b3a51d71f3
JongIn Koo <jiku90@naver.com>
2024-12-23 01:39:47 +0900

    Delete dist directory

--- dist/preload.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('electron', {
-    openFileDialog: () => electron_1.ipcRenderer.invoke('dialog:openFile'),
-    readFileContent: (filePath) => electron_1.ipcRenderer.invoke('file:readContent', filePath),
-    saveFileDialog: (data) => electron_1.ipcRenderer.invoke('dialog:saveFile', data)
-});
